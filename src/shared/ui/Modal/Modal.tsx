4d8af46c5e9f44c8afdab6e3534bb341ffd18c83
OdinTakoy <bugazov9@mail.ru>
2023-06-04 23:18:13 +0300

    add modal sigIn

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,59 @@
+import React, {
+    MutableRefObject,
+    ReactNode, useCallback, useEffect, useRef, useState
+} from 'react';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+import { useModal } from '@/shared/lib/hooks/useModal/useModal';
+import { Overlay } from '../Overlay/Overlay';
+import { Portal } from '../Portal/Portal';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+    lazy?: boolean;
+}
+
+const ANIMATION_DELAY = 300;
+
+const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+        lazy
+    } = props;
+
+    const { isMounted, isClosing, close } = useModal({
+        animationDelay: ANIMATION_DELAY,
+        isOpen,
+        onClose
+    });
+
+    const mods: Mods = {
+        [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing
+    };
+
+    if (lazy && !isMounted) {
+        return null;
+    }
+
+    return (
+        <Portal>
+            <div className={classNames(cls.Modal, mods, [className, 'app_modal'])}>
+                <Overlay onClick={close} />
+                <div
+                    className={cls.content}
+                >
+                    {children}
+                </div>
+            </div>
+        </Portal>
+    );
+};
+
+export default Modal
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,7 +1,7 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Header.module.scss';
 
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
 import Logo from '@/shared/assets/icons/logo.svg';
 import Catalog from '@/shared/assets/icons/catalog.svg';
 import { HStack } from '@/shared/ui/Stack';
@@ -12,14 +12,20 @@ import { Input } from '@/shared/ui/Input/Input';
 import LkIcon from '@/shared/assets/icons/lk-icon.svg';
 import CartIcon from '@/shared/assets/icons/shop-icon.svg';
 import FavoriteIcon from '@/shared/assets/icons/heart-icon.svg';
+import dynamic from 'next/dynamic';
+const Modal = dynamic(() => import('@/shared/ui/Modal/Modal'), { ssr: false });
 
 interface HeaderProps {
     className?: string;
 }
 
 export const Header = memo((props: HeaderProps) => {
+    const [isOpen, setIsOpen] = useState(false);
     const { className } = props;
 
+    const onClose = useCallback(() => {
+        setIsOpen(false);
+    }, []);
     return (
         <div className={classNames(cls.Header, {}, [className])}>
             <HStack justify={'between'}>
@@ -50,12 +56,15 @@ export const Header = memo((props: HeaderProps) => {
                     <Button className={cls.icon} theme={ButtonTheme.CLEAR}>
                         <Image width={40} src={CartIcon} alt={'cart'}/>
                     </Button>
-                    <Button className={cls.icon} theme={ButtonTheme.CLEAR}>
+                    <Button onClick={() => setIsOpen(true)} className={cls.icon} theme={ButtonTheme.CLEAR}>
                         <Image width={40} src={LkIcon} alt={'LK'}/>
                     </Button>
                     <Button className={cls.icon} theme={ButtonTheme.CLEAR}>
                         <Image width={40} src={FavoriteIcon} alt={'cart'}/>
                     </Button>
+                    <Modal onClose={onClose} isOpen={isOpen}>
+                        Авторизация
+                    </Modal>
 
                 </HStack>
 
