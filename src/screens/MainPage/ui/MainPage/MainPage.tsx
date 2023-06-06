d8b01ebbb17b6cffac2131774cc6a9e624d0fafd
OdinTakoy <bugazov9@mail.ru>
2023-06-06 22:00:43 +0300

    добавил баннер на главную страницу

--- src/screens/MainPage/ui/MainPage/MainPage.tsx
+++ src/screens/MainPage/ui/MainPage/MainPage.tsx
@@ -1,5 +1,6 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './MainPage.module.scss';
+import Banner from '@/shared/assets/68a5eb-1146x210.webp';
 
 import { memo } from 'react';
 import { Header } from '@/widgets/Header';
@@ -7,6 +8,8 @@ import { Page } from '@/widgets/Page/Page';
 import { Text } from '@/shared/ui/Text/Text';
 import { Container } from '@/shared/ui/Container/Container';
 import { ServiceInfo } from '@/widgets/ServiceInfo';
+import { VStack } from '@/shared/ui/Stack';
+import Image from 'next/image';
 interface MainPageProps {
     className?: string;
 }
@@ -18,10 +21,14 @@ const MainPage = memo((props: MainPageProps) => {
         <Page className={classNames(cls.MainPage, {}, [className])}>
             <Header/>
             <Container>
-                <Text
-                    className={cls.title}
-                    title={'Ешь Деревенское — сервис по доставке настоящих фермерских продуктов'}/>
-                <ServiceInfo/>
+
+                <VStack gap={'32'}>
+                    <Text
+                        className={cls.title}
+                        title={'Ешь Деревенское — сервис по доставке настоящих фермерских продуктов'}/>
+                    <ServiceInfo/>
+                    <Image className={cls.banner} src={Banner} alt={'banner'}/>
+                </VStack>
             </Container>
 
         </Page>
